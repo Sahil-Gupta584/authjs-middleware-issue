9380a45b6ee9089cf1fc1db0f1b0eb4adbda9315
Sahil Gupta <guptas3067@gmail.com>
2025-04-28 12:08:12 +0530

    first commit

---
+++ auth.ts
@@ -0,0 +1,41 @@
+import NextAuth, { NextAuthResult } from "next-auth";
+import { channelAccessScopes } from "./app/constants";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "./lib/prisma";
+
+const result = NextAuth(() => {
+  return {
+    adapter: PrismaAdapter(prisma),
+
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        allowDangerousEmailAccountLinking: true,
+        authorization: {
+          params: {
+            scope: channelAccessScopes.join(" "),
+            access_type: "offline",
+            prompt: "consent",
+          },
+        },
+        profile(profile) {
+          return {
+            email: profile.email,
+            id: profile.id,
+            name: profile.name,
+            image: profile.picture,
+            role: "CREATOR",
+          };
+        },
+      }),
+    ],
+    session: { strategy: "jwt" },
+  };
+});
+
+export const handlers: NextAuthResult["handlers"] = result.handlers;
+export const auth: NextAuthResult["auth"] = result.auth;
+export const signIn: NextAuthResult["signIn"] = result.signIn;
+export const signOut: NextAuthResult["signOut"] = result.signOut;
---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import type { NextConfig } from "next";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "i.ibb.co",
+      },
+    ],
+  },
+  env: {
+    CREATOR_BASE_URL: process.env.CREATOR_BASE_URL,
+  },
+};
+
+export default nextConfig;
